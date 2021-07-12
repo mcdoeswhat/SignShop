d761c0e2e086d01d80f9f326298d178f331cee46
mcdoeswhat <2929356483@qq.com>
2021-07-12 18:56:15 +0800

    Add files via upload

--- src/me/albert/signshop/utils/DataUtil.java
+++ src/me/albert/signshop/utils/DataUtil.java
@@ -3,31 +3,20 @@ package me.albert.signshop.utils;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Base64;
-
 public class DataUtil {
     public static String itemToString(ItemStack itemStack) {
-        itemStack.setAmount(1);
-        return Base64.getEncoder().encodeToString(itemStack.serializeAsBytes());
-    }
-
-    public static ItemStack stringToItem(String stringBlob) {
-        return ItemStack.deserializeBytes(Base64.getDecoder().decode(stringBlob));
-    }
-
-    public static String itemToStringOld(ItemStack itemStack) {
         itemStack.setAmount(1);
         YamlConfiguration config = new YamlConfiguration();
         config.set("item", itemStack);
         return config.saveToString();
     }
 
-    public static ItemStack stringToItemOld(String stringBlob) {
+    public static ItemStack stringToItem(String stringBlob) {
         YamlConfiguration config = new YamlConfiguration();
         try {
             config.loadFromString(stringBlob);
         } catch (Exception e) {
-            return null;
+            e.printStackTrace();
         }
         return config.getItemStack("item", null);
     }
